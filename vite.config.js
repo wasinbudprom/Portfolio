4ac0cd81366eb0017c0ecc34d1549e1ebf722951
wasinbudprom <wasin.budprom@hotmail.com>
2025-03-27 21:51:28 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
-  build: {
-    outDir: "build",
-  },
+  base: "/Portfolio",
 });
